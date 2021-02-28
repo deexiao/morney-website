6197674d8b9e2c87d4a62c7b8b4901d62ac56103
dixiao <mopjtv@gmail.com>
2021-02-28 10:45:31 +0800

    deploy

---
+++ precache-manifest.acce507cc27065d8f4797e78aa9a0b5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b20fb246fa0826cafa6",
+    "url": "/morney-website/css/app.eef87859.css"
+  },
+  {
+    "revision": "99bdf6c3ebdc9facf8b37b45f068d11c",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "2b20fb246fa0826cafa6",
+    "url": "/morney-website/js/app.af983f67.js"
+  },
+  {
+    "revision": "030de9b8ab896e69d57a",
+    "url": "/morney-website/js/chunk-vendors.201c89a5.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "57cf12e2c432f3420bd8d6a12932180c",
+    "url": "/morney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
